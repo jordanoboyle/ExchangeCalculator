e0e23742cd442dd6bef30f0b3658cd5427f0699b
jordanoboyle <jmcmcode@gmail.com>
2024-08-28 21:36:40 -0600

    variables from the DOM

--- exampleJson.js
+++ exampleJson.js
@@ -1,7 +1,7 @@
 [
   {"id": 1, "text": "Item One"},
   {"id": 2, "text": "Item two"},
-  {"id": 3, "text": "Item THREE"}
+  {"id": 3, "text": "THIS IS ITEM 3, AN EXAMPLE OF WHAT CAN BE DONE WITH FETCH"}
 ]
 
   
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//Right here is a basic explanation of what is going on with fetch API. 
 //Basic GET request
 function getTheData() {
   fetch('exampleJson.js')
@@ -21,3 +21,25 @@ function getTheData2() {
 getTheData2();
 
 //Example of what you can do with this (will be commented out)
+//UNCOMMENT: This will replace the whole page but it is meant to teach what we can do with fetched data
+// function getTheData3() {
+//   fetch('exampleJson.js')
+//     .then(response => response.json())
+//     .then(data => document.body.innerHTML = data[2].text);
+// }
+// getTheData3();
+
+
+//DOM Elements needed:
+const currencyElement_one = document.getElementById('currency-one');
+const amountElement_one = document.getElementById('amount-one');
+const currencyElement_two = document.getElementById('currency-two');
+const amountElement_two = document.getElementById('amount-two');
+
+const rateElement = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+
+//Fetch exchange rate and update the DOM
+
+//Event Listeners
